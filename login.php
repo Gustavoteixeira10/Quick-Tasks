9614a138d65aa3086e6fbd9a32c62f368dced136
mdxBruno <125074529+mdxBruno@users.noreply.github.com>
2024-06-06 19:58:59 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+
+// Conectar ao banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quicktasks";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+session_start();
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+
+// Obter os valores do formulário
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+// Consulta SQL para verificar se o email e a senha estão corretos
+$sql = "SELECT * FROM perfil WHERE email='$email' AND senha='$senha'";
+$result = $conn->query($sql);
+$user = $result->fetch_assoc();
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$_SESSION['id'] = $user['id'];
+
+
+if ($result->num_rows > 0) {
+    // Login bem-sucedido
+    echo "Login bem-sucedido! Bem vindo";
+    header("Location: index.php");
+} else {
+    echo "Email ou senha incorretos!";
+}
+$conn->close();
