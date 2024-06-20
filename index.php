b75c171159b91d54ef4d8f62969b633af64b2cea
YFoFuChU__ <119076236+YFoFuChU@users.noreply.github.com>
2024-06-20 13:42:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,182 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Primeiro Projeto</title>
-    <style type="text/css">
-        body {
-            margin: 20px;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QuickTasks</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css?v=<?= time() ?>">
 </head>
+<?php
+require('conexao.php');
+
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+if (!empty($_SESSION['id'])) {
+  $id_user = $_SESSION['id'];
+
+  try {
+    $sql = "SELECT * FROM perfil WHERE id = $id_user";
+    // Prepara a declaração SQL
+    $stmt = $pdo->prepare($sql);
+
+    // Executa a declaração SQL com os parâmetros bind
+    $stmt->execute();
+
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    //echo "Login não efetuado";
+  }
+
+  $busca = "";
+}
+?>
 
 <body>
-    <h1 class="text-center"> Cadastro de Aluno</h1>
 
-    <form action="insert.php" method="post">
-        <div class="container">
+  <header>
 
-            <div class="row">
+    <h1>QuickTasks</h1>
 
-                <div class="col-md-8">
-                    <label for="exampleInputText1" class="form-label">Nome</label>
-                    <input type="text" name="nome" class="form-control" id="exampleInputText1">
-                </div>
-
-
-                <div class="col-md-4">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">Digite o email aqui.</div>
-                </div>
-
-            </div>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <a href="dados.php" class="btn btn-primary">Ver dados</a>
-        </div>
-
+    <form action="busca.php" method="GET" id="busca">
+      <input type="text" name="busca" id="txt-busca">
+      <input type="submit" value="Buscar" id="btn-busca">
     </form>
 
+    <h2><a href="logar.php">LOGIN</a></h2>
+    <h2><a href="cadastrese.php">CADASTRE-SE</a></h2>
+    <h2><a href="profissional.php">SOU PROFISSIONAL</a></h2>
+    <h2><a href="contato.php">CONTATO</a></h2>
+
+    
+
+    <?php if (!empty($_SESSION['id'])) { ?>
+      <div id="perfil">
+        <div>
+          <img src="<?php if (isset($usuario)) {
+                      echo $usuario['foto_perfil'];
+                    } else {
+                      echo 'usuario.jpg';
+                    }
+                    ?>" alt="">
+        </div>
+
+        <ul id="dropdown">
+          <li><a href="perfil.php"> Editar Perfil </li>
+          <li><a href="logout.php"> Sair </a></li>
+        </ul>
+      </div>
+    <?php } ?>
+
+
+
+  </header>
+
+
+  <!-- MAIN -->
+
+
+  <main id="conteudo">
+
+    <a  href="busca.php" class="banner">
+      <div class="float">
+        Serviços 24H
+      </div>
+    </a>
+
+    <a href="busca.php?busca=eletrica" class="card eletrica" id="eletrica">
+      <p>Elétrica</p>
+      <span class="botao-card">Clique Aqui</span>
+      <div></div>
+      
+    </a>
+
+
+    <a href="busca.php?busca=hidraulica" class="card hidraulica">
+      <p>Hidráulica</p>
+      <span class="botao-card">Clique Aqui</span>
+      <div></div>
+    </a>
+
+
+    <a href="busca.php?busca=geral" class="card geral">
+    <p>Geral</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=pintor" class="card pintor">
+    <p>Pintor</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=carpinteiro" class="card carpinteiro">
+    <p>Carpinteiro</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=instalacoes" class="card instalacoes">
+    <p>Instalações</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=reformas" class="card reformas">
+    <p>Pequenas Reformas</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=moveis" class="card moveis">
+    <p>Montagem de móveis</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+    <a href="busca.php?busca=serralheiro" class="card serralheiro">
+    <p>Serralheiro</p>
+    <span class="botao-card">Clique Aqui</span>
+    <div></div>
+    </a>
+
+
+
+
+  </main>
+
+
+
+
+
+  <!-- Botao dark mode -->
+
+  <div>
+    <input type="checkbox" class="checkbox" id="chk" />
+    <label class="label" for="chk">
+      <i class="fas fa-moon"></i>
+      <i class="fas fa-sun"></i>
+      <div class="ball"></div>
+    </label>
+  </div>
+  <script defer src="script.js"></script>
+
+  <script src="https://kit.fontawesome.com/998c60ef77.js" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+<script>
+</script>
+</html>
 </body>
 
 </html>
