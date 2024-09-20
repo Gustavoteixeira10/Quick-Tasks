e56e9b6df570c1625d0b4f6dbfb3116aea0a8967
GustavoTeixeira_10 <119076236+Gustavoteixeira10@users.noreply.github.com>
2024-09-20 20:34:29 -0300

    Update logar.php

--- php/logar.php
+++ php/logar.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="logar.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="css/logar.css?v=<?= time() ?>">
 </head>
 <body>
     
