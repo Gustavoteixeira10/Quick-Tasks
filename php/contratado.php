78dac775d7e334e8e5d70867059c6b831170a500
GustavoTeixeira_10 <119076236+Gustavoteixeira10@users.noreply.github.com>
2024-10-04 23:26:05 -0300

    Create contratado.php

---
+++ php/contratado.php
@@ -0,0 +1,87 @@
+<?php
+include("conexao.php");
+
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!empty($_SESSION['id']) && isset($_SESSION['id'])) {
+    $id_user = $_SESSION['id'];
+}
+
+
+$servico = $_POST['id_servico'];
+$cliente = $_POST['id_cliente'];
+
+
+
+
+$sql = "INSERT INTO `contratados`(`id`, `id_servico`, `id_cliente`) VALUES (NULL, $servico, $cliente)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuickTasks | Serviço contratado</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/logar.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="css/contratado.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <header>
+        <h1><a href="index.php">QuickTasks</a></h1>
+
+        <div>
+            <input type="checkbox" class="checkbox" id="chk" />
+            <label class="label" for="chk">
+                <i class="fas fa-moon"></i>
+                <i class="fas fa-sun"></i>
+                <div class="ball"></div>
+            </label>
+        </div>
+
+
+        <script src="https://kit.fontawesome.com/998c60ef77.js" crossorigin="anonymous"></script>
+
+    </header>
+
+
+
+    <main>
+
+        <div>
+            <h1 style="text-align: center;">Serviço contratado!</h1>
+
+            <a href="index.php" class="btn btn-primary voltar">Voltar</a>
+        </div>
+
+    </main>
+
+</body>
+<script>
+    function pop() {
+        let popover = document.getElementById("popover")
+        popover.classList.toggle("popActive")
+    }
+</script>
+
+</html>
