a3057890fbf7c9967fe2c03787ac01599d2f03a6
GustavoTeixeira_10 <119076236+Gustavoteixeira10@users.noreply.github.com>
2024-10-04 23:32:02 -0300

    Update servico.php

--- php/servico.php
+++ php/servico.php
@@ -65,7 +65,7 @@ $comentado = $stmt;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="logar.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="css/logar.css?v=<?= time() ?>">
     <link rel="stylesheet" href="css/servico.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -95,7 +95,8 @@ $comentado = $stmt;
 
     <main>
         <section class="text-center">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu5jQ9gClnE2MsnLWClIfUtYbbAa4DNuwqkA&s" class="rounded" alt="...">
+            <img src="<?= $item['fotos']?>" class="rounded" alt="..."
+            onerror="if (this.src != 'images/default_image.png') this.src = 'images/default_image.png'">
         </section>
 
         <section>
@@ -131,31 +132,31 @@ $comentado = $stmt;
                 <input type="text" name="id_pag" value="<?php echo $id ?>" hidden>
                 <input type="text" name="nota_qnt" value="<?php echo $qnt ?>" hidden>
 
-                <button class="star" name="nota" type="submit" value="5">
+                <button class="star" name="nota" type="<?= (isset($_SESSION['id'])) == true ? "submit" : "button" ?>" value="5">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                     </svg>
                 </button>
 
-                <button class="star" name="nota" type="submit" value="4">
+                <button class="star" name="nota" type="<?= (isset($_SESSION['id'])) == true ? "submit" : "button" ?>" value="4">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                     </svg>
                 </button>
 
-                <button class="star" name="nota" type="submit" value="3">
+                <button class="star" name="nota" type="<?= (isset($_SESSION['id'])) == true ? "submit" : "button" ?>" value="3">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                     </svg>
                 </button>
 
-                <button class="star" name="nota" type="submit" value="2">
+                <button class="star" name="nota" type="<?= (isset($_SESSION['id'])) == true ? "submit" : "button" ?>" value="2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                     </svg>
                 </button>
 
-                <button class="star" name="nota" type="submit" value="1">
+                <button class="star" name="nota" type="<?= (isset($_SESSION['id'])) == true ? "submit" : "button" ?>" value="1">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                     </svg>
@@ -165,16 +166,6 @@ $comentado = $stmt;
 
             <div class="btn-pagamento">
                 <form action="" method="post">
-                    <!-- <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Métodos de pagamento
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Pix</a></li>
-                            <li><a class="dropdown-item" href="#">Crédito</a></li>
-                            <li><a class="dropdown-item" href="#">Débito</a></li>
-                        </ul>
-                    </div> -->
                     <button type="button" class="btn btn-primary" onclick="pop()">Métodos de pagamento</button>
                     <div id="popover">
                         <h3>Aceitamos:</h3>
@@ -186,7 +177,7 @@ $comentado = $stmt;
                         <button class="btn btn-primary" onclick="pop()">Fechar</button>
                     </div>
 
-                    <button type="button" class="btn btn-secondary">Contratar</button>
+                    <a href="contratar.php?servico=<?= $id ?>" type="button" class="btn btn-secondary">Contratar</a>
                 </form>
             </div>
 
